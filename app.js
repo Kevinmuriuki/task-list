1b8424d3cff12e6a2ab382b918200259d67c00e5
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-09-01 10:34:40 +0300

    added remove task event.

--- app.js
+++ app.js
@@ -43,3 +43,12 @@ function addTask(e) {
   e.preventDefault();
 }
 
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains("delete-item")) {
+    if(confirm("Are you sure you want to delete?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+
+}
